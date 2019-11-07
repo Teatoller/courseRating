2f4ca78151bc250a65fc3440e89b8ef4158c483f
Teatoller <steven.otieno1@gmail.com>
2019-11-07 11:02:10 +0300

    (update rating): Implement rating update
    - add event handle to update rating
    - pass event handle props to star component
    - add onclick event on star icon
    - add style props to highlight to selected rating
    - add ternary style class to start icon
    - add conditional to reset rating when reclicked upon
    
    [Starts #004]

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -9,6 +9,7 @@ class StarRating extends Component {
       rating: 0
     };
     this.renderStars = this.renderStars.bind(this);
+    this.handleUpdateRating = this.handleUpdateRating.bind(this);
   }
 
   // Write a function that returns 5 Star components
@@ -17,13 +18,29 @@ class StarRating extends Component {
     let maxRating = 5;
 
     for (let i = 0; i < maxRating; i++) {
-      stars.push(<Star key={i} />);
+      stars.push(
+        <Star
+          setRating={() => this.handleUpdateRating(i + 1)}
+          isSelected={this.state.rating > i}
+          key={i}
+        />
+      );
     }
     return stars;
   }
 
   // Write an event handler that updates the rating state
   //    Pass the function to a Star component via props
+
+  handleUpdateRating(rating) {
+    if (this.state.rating === rating) {
+      this.setState({ rating: 0 });
+    } else {
+      this.setState({
+        rating: rating
+      });
+    }
+  }
 
   render() {
     console.log(this.state);
